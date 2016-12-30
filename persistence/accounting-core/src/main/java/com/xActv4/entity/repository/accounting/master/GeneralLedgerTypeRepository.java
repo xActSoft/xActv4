777b8c2bb69f197311f4187de9e6127fab02c0c3
exactuser1 <exactuser1@gmail.com>
2016-12-30 15:46:24 -0500

    Refactoring CRUD Test

--- persistence/accounting-core/src/main/java/com/xActv4/entity/repository/accounting/master/GeneralLedgerTypeRepository.java
+++ persistence/accounting-core/src/main/java/com/xActv4/entity/repository/accounting/master/GeneralLedgerTypeRepository.java
@@ -1,6 +1,8 @@
 package com.xActv4.entity.repository.accounting.master;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.xActv4.entity.accounting.master.GeneralLedgerType;
@@ -8,4 +10,7 @@ import com.xActv4.entity.accounting.master.GeneralLedgerType;
 @Repository
 public interface GeneralLedgerTypeRepository extends CrudRepository<GeneralLedgerType, Long> {
 
+	@Query("SELECT glType FROM GeneralLedgerType glType WHERE LOWER(glType.systemCode) = LOWER(:syscode)")
+	public GeneralLedgerType findGeneralLedgerTypeBySysCode(@Param("syscode") String syscode);
+	
 }
